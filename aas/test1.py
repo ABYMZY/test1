2875ef828d006af8e44ab50b4385e9c2fdd52f63
user <abc@gmail.com>
2023-01-26 11:48:25 +0330

    name

--- aas/test1.py
+++ aas/test1.py
@@ -1,6 +1,14 @@
-thisdict = {
-"brand": "Ford",
-"model": "Mustang",
-"year": 1964
-}
-print(thisdict)
+class Person:
+  def __init__(self, fname, lname):
+   self.firstname = fname
+   self.lastname = lname
+  def printname(self):
+   print(self.firstname, self.lastname)
+x = Person("John", "Doe")
+x.printname()
+
+
+class Student(Person):
+  pass
+x = Student("Mike", "Olsen")
+x.printname()
