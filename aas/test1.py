4ebb99762a8fe38586f2029c8fa36e4a7dd3ba17
user <abc@gmail.com>
2023-02-02 08:35:54 +0330

    name

--- aas/test1.py
+++ aas/test1.py
@@ -1,13 +1,2 @@
-f = open("test.txt", "w+")
-f.write("1Now the file has more content!\n")
-f.write("2Now the file has more content!\n")
-f.write("3Now the file has more content!\n")
-f.write("4Now the file has more content!\n")
-f.write("5Now the file has more content!\n")
-f.write("6Now the file has more content!\n")
-f.write("7Now the file has more content!")
-
-with open('test.txt', "r") as f:
-    lines = [line.rstrip() for line in f]
-[print(i) for i in lines]
-# print(f.read())
+import mymodule
+mymodule.greeting("Jonathan")
