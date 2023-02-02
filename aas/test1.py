49602b582fb7cd052438720a4e0dd7e457a80de0
user <abc@gmail.com>
2023-02-02 10:57:29 +0330

    name

--- aas/test1.py
+++ aas/test1.py
@@ -1,14 +1,4 @@
-import json
-x = {
-"name": "John",
-"age": 30,
-"married": True,
-"divorced": False,
-"children": ("Ann","Billy"),
-"pets": None,
-"cars": [
-{"model": "BMW 230", "mpg": 27.5},
-{"model": "Ford Edge", "mpg": 24.1}
-]
-}
-print(json.dumps(x))
+import re
+txt = "The rain in Spain"
+x = re.sub("\s", "9", txt, 4)
+print(x)
