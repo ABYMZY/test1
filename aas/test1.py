33731bb2cdc1587141b36b4ba9bca44a53f31b43
user <abc@gmail.com>
2023-02-02 09:40:34 +0330

    name

--- aas/test1.py
+++ aas/test1.py
@@ -1,2 +1,10 @@
-import mymodule
-mymodule.greeting("Jonathan")
+try:
+  f = open("test.txt")
+  try:
+   f.write("Lorum Ipsum")
+  except:
+   print("Something went wrong when writing to the file")
+  finally:
+   f.close()
+except:
+ print("Something went wrong when opening the file")
